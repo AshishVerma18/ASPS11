e8865754fe9e070ed4abd5ef02b37905f89aaa25
AshishVerma18 <aashish.640@gmail.com>
2021-01-22 13:20:18 +0530

    dt

---
+++ class/JSBasics/Datatypes.js
@@ -0,0 +1,12 @@
+let stud_name='Rohan';
+let stud_age= 30;
+let isPresent= true;
+let stud_marks=null;
+console.log(stud_name,
+    stud_age,
+    isPresent,
+    stud_marks);
+console.log(typeof stud_name,
+    typeof stud_age,
+    typeof isPresent,
+    typeof stud_marks);
