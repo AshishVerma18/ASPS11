696227a7040d68f8a7a69e6df5b1a05a06f0f9b8
AshishVerma18 <aashish.640@gmail.com>
2021-01-23 15:10:23 +0530

    'old'

---
+++ class/JSBasics/Assign1.js
@@ -0,0 +1,13 @@
+/*let number=21;
+if(number%7==0){
+	console.log('the number is divisble by 7')
+}
+else
+	console.log('the number is not devisble by 7');
+*/
+let number=21;
+if(number%7==0){
+	console.log('the number is divisble by 7')
+}
+else
+	console.log('the number is not devisble by 7');
