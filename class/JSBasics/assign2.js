2cf978d481ccdf5021493d3e903b7ef152972b1c
AshishVerma18 <aashish.640@gmail.com>
2021-01-23 15:09:43 +0530

    'new'

---
+++ class/JSBasics/assign2.js
@@ -0,0 +1,13 @@
+/*let number=21;
+if(number%3==0){
+	console.log('the number is multiple of 3')
+}
+else
+	console.log('the number is not multiple of 3');
+*/
+let number=29;
+if(number%3==0){
+	console.log('the number is multiple of 3')
+}
+else
+	console.log('the number is not multiple of 3');
