ce78557284524b1cff304ddd7a2599300b00ecf0
AshishVerma18 <aashish.640@gmail.com>
2021-01-23 15:24:57 +0530

    '4'

---
+++ class/JSBasics/assign4.js
@@ -0,0 +1,14 @@
+/*let num=1235;
+if(num%10==4){
+	console.log("the number has 4 at unit's place");
+}
+else 
+	console.log("the number doesn't has 4 at unit's place");
+*/
+
+let num=1235;
+if(num%10==4){
+	console.log("the number has 4 at unit's place");
+}
+else 
+	console.log("the number doesn't has 4 at unit's place");
