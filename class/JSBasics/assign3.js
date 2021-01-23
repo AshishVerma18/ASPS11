14e6bc8ba68548cc0df26945843eebec06d8b6cb
AshishVerma18 <aashish.640@gmail.com>
2021-01-23 15:19:21 +0530

    '1'

---
+++ class/JSBasics/assign3.js
@@ -0,0 +1,19 @@
+/*let num=-3;
+if(num>=0){
+	console.log('Number is Positive');
+}
+else if(num<0){
+	console.log('Number is negative');
+	
+}
+*/
+
+
+let num=13;
+if(num>=0){
+	console.log('Number is Positive');
+}
+else if(num<0){
+	console.log('Number is negative');
+	
+}
