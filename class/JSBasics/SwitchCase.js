107eac40392f2d483fa18e0cbb0bd43250cfcac8
AshishVerma18 <aashish.640@gmail.com>
2021-01-23 14:22:31 +0530

    '2'

---
+++ class/JSBasics/SwitchCase.js
@@ -0,0 +1,35 @@
+
+
+let msg='outgoing call';
+switch(msg){
+	case 'incoming call':
+	    
+		console.log('someone is calling');
+	break;
+	case 'outgoing call':
+		
+		console.log('Call is being connected');
+	break;
+	default:
+		console.log('please call');
+}
+
+
+
+
+/*
+let msg='yyy';
+switch(msg){
+	case 'GM':
+		console.log('Hello, Good Morning');
+	break;
+	case 'GA':
+		console.log('Hello,Good Afternoon');
+	break;
+	case 'GN':
+		console.log('Hello,Good Night');
+	break;
+	default:
+		console.log('Please Wish');
+}
+*/
