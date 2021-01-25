b91a71c2090a5ea08bced39502f53ec9f2c312e3
AshishVerma18 <aashish.640@gmail.com>
2021-01-25 14:57:06 +0530

    '9'

---
+++ class/JSBasics/Assignment/assign9.js
@@ -0,0 +1,22 @@
+//greatest number in given three numbers....
+
+let num1=1000;
+let num2=34;
+let num3=500;
+if(num1>num2){
+	if(num1>num3){
+	
+	console.log('greatest number is :',num1);
+    }
+}
+else if(num2>num1){
+	if(num2>num3){
+	console.log('greatest number is:',num2);
+    }
+else
+	console.log('greatest number is:',num3);
+}
+
+
+
+
