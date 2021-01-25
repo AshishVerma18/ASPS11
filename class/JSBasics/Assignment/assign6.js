ae47353451359b52e1968e32df1656bb359c4a7b
AshishVerma18 <aashish.640@gmail.com>
2021-01-25 14:55:54 +0530

    '6'

---
+++ class/JSBasics/Assignment/assign6.js
@@ -0,0 +1,16 @@
+// Odd Number check.....
+let num=35;
+if(num%2==0){
+	console.log('given num is even');
+}
+else
+	console.log('given number is odd');
+// Even Number Check.....
+
+
+let num1=38;
+if(num1%2==0){
+	console.log('given num is even');
+}
+else
+	console.log('given number is odd');
