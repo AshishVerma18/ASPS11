849e512d68fe905034f440d9206703c9a93405a3
AshishVerma18 <aashish.640@gmail.com>
2021-01-25 14:55:18 +0530

    '5'

---
+++ class/JSBasics/Assignment/assign5.js
@@ -0,0 +1,19 @@
+let num=13;
+if(num>=100){
+	if(num<=999){
+		console.log('number is three digitnumber');
+	}
+}
+else
+	console.log('number is not a three digit number');
+
+
+let num1=138;
+if(num1>=100){
+	if(num1<=999){
+		console.log('number is three digitnumber');
+	}
+}
+else
+	console.log('number is not a three digit number');
+
