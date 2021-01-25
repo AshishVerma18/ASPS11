a05e4bae1aaff4f2127c1e67bfb2d60f2f2a2b32
AshishVerma18 <aashish.640@gmail.com>
2021-01-25 14:57:50 +0530

    '10'

---
+++ class/JSBasics/Assignment/assign10.js
@@ -0,0 +1,18 @@
+// least of three numbers........
+
+let num1=1;
+let num2=34;
+let num3=5;
+if(num1<num2){
+	if(num1<num3){
+	
+	console.log('least number is :',num1);
+    }
+}
+else if(num2<num1){
+	if(num2<num3){
+	console.log('least number is :',num2);
+    }
+else
+	console.log('gleast number is :',num3);
+}
