589f69371e68ae1351817f5415f717f84c0e2963
AshishVerma18 <aashish.640@gmail.com>
2021-01-25 14:56:21 +0530

    '7'

---
+++ class/JSBasics/Assignment/assign7.js
@@ -0,0 +1,8 @@
+// greater num in given two numbers....
+let num1=27678;
+let num2=8764;
+if(num1>num2){
+	console.log('the greater number is:',num1);
+}
+else
+	console.log('the greater number is :',num2);
