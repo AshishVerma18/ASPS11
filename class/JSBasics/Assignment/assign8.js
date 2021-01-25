ae286b9379cefa2d5338142aaad147aed1ba9a8f
AshishVerma18 <aashish.640@gmail.com>
2021-01-25 14:56:43 +0530

    '8'

---
+++ class/JSBasics/Assignment/assign8.js
@@ -0,0 +1,9 @@
+//least number in given two numbers....
+
+let num1=89;
+let num2=450;
+if(num1>num2){
+	console.log('the least number is:',num2);
+}
+else
+	console.log('the least number is :',num1);
